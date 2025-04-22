9d7497867ed6a24535cf24e82989c4cd790d1e18
brunoviera0 <144165470+brunoviera0@users.noreply.github.com>
2025-04-22 13:05:09 -0400

    Create schema.py

---
+++ schema.py
@@ -0,0 +1,16 @@
+schema = {
+  "name": "ssga_detail",
+  "fields": [
+    { "name": "Period", "type": "string" },
+    { "name": "Category", "type": "string", "facet": True },
+    { "name": "DocumentType", "type": "string" },
+    { "name": "Keyword", "type": "string", "facet": True },
+    { "name": "Paragraph", "type": "string" },
+    { "name": "Score", "type": "float" },
+    { "name": "Magnitude_Score", "type": "float" },
+    { "name": "Ticker", "type": "string", "facet": True }
+  ],
+  "default_sorting_field": "Score"
+}
+
+typesense_client.collections.create(schema)
